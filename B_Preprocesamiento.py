47ffd673a5850ecf8338e65797c2ae1fcda58d57
JorgeRodriguez <jorge.rodriguez1@udea.edu.co>
2024-02-25 17:29:45 -0500

    Actualización conexión de rutas

--- B_Preprocesamiento.py
+++ B_Preprocesamiento.py
@@ -1,1 +1,13 @@
 # Este apartado será utilizado para realizar el proprocesamiento de los datos 
+
+# Librerias necesarias 
+import pandas as pd 
+import sqlite3 as sql 
+import A_Funciones as funciones
+import sys 
+
+# Agregar la ruta que contiene el archivo de funciones
+sys.path
+sys.path.append('C:\\Users\\jorge\\Desktop\\Proyecto RRHH')
+sys.path
+
