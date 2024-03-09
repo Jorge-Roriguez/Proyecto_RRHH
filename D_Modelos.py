655ba44f8beba1f88a2e2e8031e44f0701904c75
EstebanCaroP <esteban.carop@udea.edu.co>
2024-03-09 12:05:47 -0500

    Afinamiento de hiperparametros para XGBoost

--- D_Modelos.py
+++ D_Modelos.py
@@ -134,3 +134,34 @@ f1_var_sel['xgboost_classifier'].mean()
 # Observamos que el mejor desempeño lo tiene un XGBoost Classifier con el conjunto de variables 
 # seleccionadas. Se hará optimización de hiperparámetros para dicho algoritmo para dicho conjunto de datos. 
 
+
+# -------------------------------------- Afinamiento de hiperparámetros XGBoost ------------------------------- #
+
+m_xgb = XGBClassifier()
+
+hiper = m_xgb.get_params()
+hiper
+
+param_grid = [{'max_depth': [3,4,5,6], 'scale_pos_weight': [6.44], 
+               'eta':[0.01, 0.09, 0.1, 0.2],'subsample': [0.5,0.7,0.8,1] }]
+
+
+# max_depth para evitar sobreajuste
+# scale_pos_weight: Control the balance of positive and negative weights, useful for unbalanced classes.
+# A typical value to consider: sum(negative instances) / sum(positive instances)
+
+# En nuestro caso, no renuncian: 3769 y renuncian: 585
+# scale_pos_weight = 3769/585 = 6.44
+
+## binary:hinge: hinge loss for binary classification. 
+# This makes predictions of 0 or 1, rather than producing probabilities.
+
+
+tun_rf=RandomizedSearchCV(m_xgb,param_distributions=param_grid,n_iter=10,scoring="f1")
+tun_rf.fit(X2,y)
+
+pd.set_option('display.max_colwidth', 100)
+resultados=tun_rf.cv_results_
+tun_rf.best_params_
+pd_resultados=pd.DataFrame(resultados)
+pd_resultados[["params","mean_test_score"]].sort_values(by="mean_test_score", ascending=False)
