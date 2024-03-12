6f23c6f95c3840f56ced124da8e8a4ca7fa2b213
JorgeRodriguez <jorge.rodriguez1@udea.edu.co>
2024-03-12 12:59:57 -0500

    Variables y predicciones para despliegue

--- E_Despliegue.py
+++ E_Despliegue.py
@@ -22,14 +22,50 @@ curr = conn.cursor()
 # Leer datos para 2016 
 df_2016 = pd.read_sql("SELECT * FROM tabla_2016", conn)
 
-# -------------------------- Aplicar transformaciones a los datos (Imputar, Dummies, Escalar) ----
+# -------------------- Aplicar transformaciones a los datos (Imputar, Dummies, Escalar) ----
 
 df_t = funciones.preparar_datos(df_2016)
 df_t
 
 
+# -------------------------- Cargar modelo y realizar predicciones -------------------------
+
+m_xg = joblib.load('Salidas\\xg_final.pkl')
+predicciones = m_xg.predict(df_t)
+df_pred = pd.DataFrame(predicciones, columns = ['pred_retiros_2017'])
 
 
+# -------------------------- Dataframe completo con predicciones ---------------------------------
+
+# Datos no escalados para interpretar 
+df_retiros_var = df_2016[['EmployeeID', 'Department', 'Age', 'MonthlyIncome', 'EnvironmentSatisfaction',
+                           'TrainingTimesLastYear', 'JobSatisfaction', 'NumCompaniesWorked',
+                           'WorkLifeBalance', 'DistanceFromHome', 'PercentSalaryHike',
+                           'StockOptionLevel', 'YearsAtCompany', 'JobInvolvement', 
+                           'BusinessTravel', 'Gender']]
+
+df_retiros_var = pd.concat([df_retiros_var, df_pred], axis = 1)
+ 
+
+# -------------------------- Llevar datos a BD para despliegue -------------------------------------------------------------
+
+# Retiros de los empleados por departamento (Diseño de la solución)
+df_retiros_var.loc[:, ['EmployeeID', 'Department', 'pred_retiros_2017']].to_sql('retiros_pred', conn, if_exists = 'replace', index = False)
 
 
+# -------------------------- Predicciones por departamento -----------------------------------
 
+retiros = df_retiros_var[df_retiros_var['pred_retiros_2017'] == 1]
+retiros_dep = retiros.groupby(['Department'])[['pred_retiros_2017']].count().reset_index()
+retiros_dep
+
+
+# -------------------------- Exportar los resultados de interés --------------------------
+
+# Retiros por departamentos
+df_predicciones_2017 = retiros[['EmployeeID', 'Department', 'pred_retiros_2017']]
+df_predicciones_2017.set_index('EmployeeID', inplace = True) 
+df_predicciones_2017.to_excel('Salidas\\df_predicciones_2017.xlsx')
+
+
+# -------------------------- Análsis exploratorio de los resultados --------------------------
